c15f07a16c6a9087fd3709bd329006922c2206e1
binggre <wowzzangga@gmail.com>
2017-10-04 14:19:56 +1100

    Add custom environment varialbes

--- src/base.js
+++ src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyBAxcb8t5cs9lv1SM2qWmaYQwtk_uaaKnc",
-    authDomain: "my-recipe-app-e35c4.firebaseapp.com",
-    databaseURL: "https://my-recipe-app-e35c4.firebaseio.com",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 });
 
 const base = Rebase.createClass(app.database());
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {
     BrowserRouter as Router,
     Route,
   } from 'react-router-dom'
-  
+
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
